417e6a0e2ae1e8c4e859ef0ac4837a26ca7d6eb3
Siddharthin <117194139+Siddharthin@users.noreply.github.com>
2026-01-17 11:31:50 +0530

    initial commit

---
+++ variable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()  {
+    int num = 22;
+    char star = 's';
+    int age = 199;
+    float pi = 3.14f ;
+
+    printf("num  is %d\n", num);
+    printf("star is %c\n", star);
+    printf("age is %d\n",age);
+    printf("pi is %f\n", pi);
+    printf("this are format specifiers in C language");
+
+    return 0;
+ 
+}
