cdf678d20d4d94139f5271e24b95f65a8773729a
Siddharthin <117194139+Siddharthin@users.noreply.github.com>
2026-01-18 13:18:51 +0530

    second commit

---
+++ logicaloperator.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+
+int main()
+{
+    printf("%d\n", 4>3&&3<5);
+       printf("%d\n", 4>3||3<5);
+
+            printf("%d\n", !(4>3&& 3<5));
+
+
+
+    return 0;
+}
---
+++ precedence.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int X = 5!=3;
+    printf("%d\n", X);
+
+    int y = 4!=3;
+    printf("%d\n", y);
+
+    int b = 5!=3;
+    printf("%d\n", b);
+
+    int v = 3!=3;
+    printf("%d\n", v);
+
+
+
+    return 0;
+}
